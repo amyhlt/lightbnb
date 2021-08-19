485d6424b113b05a71fb6b451a5f9c69c77367b2
AmyTang <huilitang1@gmail.com>
2021-08-19 22:35:00 +0000

    update addProperty by pg

--- lightbnb_webapp/server/database.js
+++ lightbnb_webapp/server/database.js
@@ -86,7 +86,7 @@ const getAllReservations = function (guest_id, limit = 10) {
     .query(queryString, values)
     .then(res => res.rows)
     .catch(err => console.log(err.stack));
-      
+
 }
 exports.getAllReservations = getAllReservations;
 
@@ -98,15 +98,38 @@ exports.getAllReservations = getAllReservations;
  * @param {*} limit The number of results to return.
  * @return {Promise<[{}]>}  A promise to the properties.
  */
- const getAllProperties = (options, limit = 10) => {
-   const queryString =`SELECT properties.*,avg(property_reviews.rating) AS average_rating
-   FROM properties
-   JOIN property_reviews on properties.id=property_reviews.property_id
-   GROUP BY properties.id
-          LIMIT $1`;
-  return pool
-    .query(queryString, [limit])
-    .then((res) => res.rows)
+const getAllProperties = (options, limit = 10) => {
+  // 1
+  const queryParams = [];
+  // 2
+  let queryString = `
+  SELECT properties.*, avg(property_reviews.rating) as average_rating
+  FROM properties
+  JOIN property_reviews ON properties.id = property_id
+  `;
+
+  // 3
+  if (options.city) {
+    queryParams.push(`%${options.city}%`);
+    queryString += `WHERE city LIKE $${queryParams.length} `;
+  }
+
+  // 4
+  queryParams.push(limit);
+  queryString += `
+  GROUP BY properties.id
+  ORDER BY cost_per_night
+  LIMIT $${queryParams.length};
+  `;
+
+  
+  
+
+
+
+
+
+  return pool.query(queryString, queryParams).then((res) => res.rows)
     .catch((err) => {
       console.log(err.message);
     });
@@ -128,14 +151,14 @@ const addProperty = function(property) {
     thumbnail_photo_url,
     cover_photo_url,
     cost_per_night,
+    parking_spaces,
+    number_of_bathrooms,
+    number_of_bedrooms,
+    country,
     street,
     city,
     province,
     post_code,
-    country,
-    parking_spaces,
-    number_of_washrooms,
-    number_of_bedrooms,
     active)
   VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15)
   RETURNING *;
@@ -147,14 +170,14 @@ const addProperty = function(property) {
     property.thumbnail_photo_url,
     property.cover_photo_url,
     property.cost_per_night,
+    property.parking_spaces,
+    property.number_of_bathrooms,
+    property.number_of_bedrooms,
+    property.country,
     property.street,
     property.city,
     property.province,
     property.post_code,
-    property.country,
-    property.parking_spaces,
-    property.number_of_bathrooms,
-    property.number_of_bedrooms,
     true
 ];
 
